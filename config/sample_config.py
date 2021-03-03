3af4a8762458cff24a30ab3cdc8f2b65a6337f27
TioDexty <79750723+TioDexty@users.noreply.github.com>
2021-03-03 18:52:35 -0300

    Update sample_config.py

--- config/sample_config.py
+++ config/sample_config.py
@@ -9,26 +9,27 @@ API_HASH = "6342aa5989c215f0007c68e542fce52b"
 TOKEN = "1609689780:AAHnFXbmOpD1CNbq0Gjc41L-bCLVwfWIdWM"
 
 # Your MongoDB URI (using a database named "vcpb"), if you don't provide, you can't use the playlist feature and wont see now playing message
-MONGO_DB_URI = ""
+MONGO_DB_URI = "mongodb+srv://vcpb:vcpb@cluster0.nxvqr.mongodb.net/vcpb?retryWrites=true&w=majority"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
     957539786,
-    951435494,
-    1392620345
+    1620606850,
+    1417315424,
+    1402179007
 ]
 
 # The ID of the group where your bot streams
 GROUP = -1001402753006
 
 # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
-USERS_MUST_JOIN = False
+USERS_MUST_JOIN = true
 
 # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
 LANG = "en"
 
 # Max video duration allowed for user downloads in minutes
-DUR_LIMIT = 5
+DUR_LIMIT = 15
 
 # No need to touch the following.
 LOG_GROUP = GROUP if MONGO_DB_URI != "" else None
